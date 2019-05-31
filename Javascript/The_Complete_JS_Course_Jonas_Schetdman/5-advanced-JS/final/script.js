dc1d2ccd3b2b38ceb6f712b0d643454139e111cb
Didier Ukanda <didier.ukanda@gmail.com>
2019-05-31 15:42:05 -0300

    Closures InterviewQuestion

--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/5-advanced-JS/final/script.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/5-advanced-JS/final/script.js
@@ -506,3 +506,19 @@ c) correct answer (I would use a number for this)
 // }
 
 // interviewQuestions()('didier')
+
+// Lecture: Functions returning functions with closures
+
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('Didier')

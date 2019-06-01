49dfe12728a79e33492c8b355b4f79db33b8c1ab
Didier Ukanda <didier.ukanda@gmail.com>
2019-06-01 18:20:42 -0300

    Section6: Creating initialisation function

--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/6-budgety/starter/app.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/6-budgety/starter/app.js
@@ -1,5 +1,5 @@
 
-//* DATA MODULE
+//* DATA MODULE -- BUDGET CONTROLLER
 // todo Add the new item  to our data structure 
 // todo Calculate the budget 
 var budgetController = (function(){
@@ -8,18 +8,75 @@ var budgetController = (function(){
     
 })();
 
-//* UI MODULE
-// todo Get input values
-// todo Add the new item to the UI 
-// todo Update the UI
+//* UI MODULE -- UI CONTROLLER
+
 
 var UIController = (function(){
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //? Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMStrings: function() {
+            return DOMstrings;
+        }
+    }
+
 })();
 
 
-//* CONTROLLER MODULE
+//* CONTROLLER MODULE -- GLOBAL APP CONTROLLER --@params events delegates to other controllers
 // todo Add event Handler
 var controller = (function(budgetCtrl, UICtrl){
+ 
+    setupEventListeners = function () {
+        var DOM = UIController.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+    
+        document.addEventListener('keypress', function(event){
+            //? console.log(event.code === 'Enter' ? 'Validated' : 'Error'); one way todo things
+            if (event.keyCode === 13 || event.which == 13) {
+                ctrlAddItem();
+            }
+            
+        })
+    }
+
+    var ctrlAddItem = function() {
+        
+        // todo 1. Get input values
+        var input = UICtrl.getInput();
+        console.log(input);
+        
+        // todo 2. Add the new item to the UI 
+    
+        // todo 3. Add the item to the budget controller 
+    
+        // todo 4. Calculate the budget
+    
+        // todo 5. Display the budget on the UI
+        console.log('one button was pressed')
+    }
+
+    return {
+        init: function() {
+            console.log('the Application has Started');
+            setupEventListeners();
+        }
+    }
     
 })(budgetController, UIController);
+
+controller.init();

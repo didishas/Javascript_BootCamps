0b8938b4fb8b39230d2414a48950106932d87ee2
Didier Ukanda <didier.ukanda@gmail.com>
2019-06-01 13:13:26 -0300

    Section: Module-SetUp_Budgety

---
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/6-budgety/starter/app.js
@@ -0,0 +1,25 @@
+
+//* DATA MODULE
+// todo Add the new item  to our data structure 
+// todo Calculate the budget 
+var budgetController = (function(){
+    
+    
+    
+})();
+
+//* UI MODULE
+// todo Get input values
+// todo Add the new item to the UI 
+// todo Update the UI
+
+var UIController = (function(){
+    
+})();
+
+
+//* CONTROLLER MODULE
+// todo Add event Handler
+var controller = (function(budgetCtrl, UICtrl){
+    
+})(budgetController, UIController);

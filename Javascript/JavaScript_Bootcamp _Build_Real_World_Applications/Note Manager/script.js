b74d71edc4428396c56a34551c213f3531d05d07
didishas <didier.ukanda@gmail.com>
2019-06-07 18:00:35 -0300

    Note-Manager: v 1.0.0

--- Javascript/JavaScript_Bootcamp _Build_Real_World_Applications/Note Manager/script.js
+++ Javascript/JavaScript_Bootcamp _Build_Real_World_Applications/Note Manager/script.js
@@ -54,7 +54,10 @@ var UIController = (function(){
 
     var Domstring = {
         button: 'add-btn',
-        inputText: 'add-input'
+        inputText: 'add-input',
+        unorderedList: 'ul',
+        editIcon: 'fa-pencil-square-o',
+        removeIcon: 'fa-times'
     }
     var valeur;
 
@@ -76,9 +79,95 @@ var UIController = (function(){
                         </li>`
             document.querySelector(element).insertAdjacentHTML('afterend', note)
         },
+        deleteNote: function () {
+            var ul = document.querySelector(Domstring.unorderedList);
+
+            ul.addEventListener('click', function(e){
+                e.preventDefault();
+
+                if(e.target.classList[1] === Domstring.removeIcon)
+                {
+                    var note = e.target.parentNode.parentNode;
+                    console.log(e.target.parentNode.parentNode);
+                    note.remove();
+
+                }
+                
+            })
+        },
         clearInput: function(){
             document.getElementById(Domstring.inputText).value = '';
             document.getElementById(Domstring.inputText).focus();
+        },
+        editInput: function(){
+            var ul = document.querySelector(Domstring.unorderedList);
+
+            ul.addEventListener('click', function(e){
+                e.preventDefault();
+                var note, editInput, editingIcons;
+
+                editingIcons = e.target.parentNode;
+                note = e.target.parentNode.previousElementSibling;
+                editInput = e.target.parentNode.nextElementSibling;
+                
+                // make editing icons disappear
+                if(e.target.classList[1] === Domstring.editIcon) {
+                    console.log(e.target.classList[1]);
+                    
+                    editInput.style.display = 'block';
+                    editInput.value = note.textContent;
+                    editingIcons.style.display = 'none';
+
+                    editInput.focus();
+
+                    editInput.addEventListener('keypress', function(e){
+                        if (e.keyCode === 13) {
+                            // console.log(e)
+                            
+                            note.textContent = editInput.value;
+                            editInput.style.display = 'none';
+                            editingIcons.style.display = 'block';
+                        }
+                    })
+                }
+                // console.log(note.textContent) just for testing purposes
+
+                // console.log(editInput.value);
+                
+            });
+        },
+        hidingNotes: function(){
+            var ul, hidebutton, label;
+            ul = document.querySelector(Domstring.unorderedList);
+            label = document.querySelector('label');
+            hidebutton = document.getElementById('hide')
+
+            hidebutton.addEventListener('click', function(){
+                ul.style.display = hidebutton.checked ? 'none' : 'block';
+                label.textContent = hidebutton.checked ? 'Unhide note' : 'Hide note';
+            })
+
+        },
+        filterNotes: function() {
+            var searchInput = document.querySelector('#search-note input');
+            var ul = document.querySelector(Domstring.unorderedList);
+
+            searchInput.addEventListener('keyup', function(e){
+
+                var searchChar = e.target.value.toLowerCase();
+                var notes = ul.getElementsByTagName('li');
+                console.log(searchChar);
+                Array.from(notes).forEach( note => {
+                    var parText = note.firstElementChild.textContent;
+
+                    if (parText.toLowerCase().indexOf(searchChar) !== -1) {
+                        note.style.display = 'block';
+                    } else {
+                        note.style.display = 'none';                        
+                    }
+                })
+                
+            })
         }
     }
     
@@ -97,24 +186,44 @@ var Controller = (function(uiCtrl, dataCtrl){
             addNote(e);
             // console.log(e.target);
             uiCtrl.clearInput();
-        });
-                
-
+        }); 
+        editNote();
+        eraseNote();
+        uiCtrl.filterNotes()
+        uiCtrl.hidingNotes();               
     }
-            var addNote = function(e) {
-                e.preventDefault();
-                // take the Text input
-                var input = uiCtrl.getInput();
-                // console.log(input.value)
-                
-                // add the note in the data structure
-                uiCtrl.createNote('li:last-child', input.value)
-            }
+
+    var addNote = function(e) {
+        e.preventDefault();
+        // take the Text input
+        var input = uiCtrl.getInput();
+        // console.log(input.value)
+        
+        // add the note in the data structure
+        uiCtrl.createNote('li:last-child', input.value)
+    }
+
+    var editNote = function() {
+
+        // make the editing input appear
+        uiCtrl.editInput();
+        // todo with the note inside 
+
+        // edit the note and save it
+    }
+
+    var eraseNote = function() {
+        uiCtrl.deleteNote();
+    }
 
     return {
         init: function(){
             SetEventListeners();
         }
+    }
+
+    var filteringNotes = function(){
+        uiCtrl.filterNotes()
     }
     
 

16ab47052831f7453bc765adbbdabd8ec98c2032
Didier Ukanda <didier.ukanda@gmail.com>
2019-06-18 07:34:24 -0300

    Forkify: First module Es6 import

--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/index.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/index.js
@@ -1,4 +1,10 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js. Variable x is ${x}`);
+//! first way to import thing as simple files
+import str from './models/Search'
+
+//! second way import thing as aliases
+// import {Add as a , Multiply as m, Substract as s, Divide as d, ID} from './views/searchView'
+
+//! Third way alias the Path
+import * as searchView  from './views/searchView'
+
+console.log(`Using the Imported functions ${searchView.Add(searchView.ID, 2)}`)
---
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/views/searchView.js
@@ -0,0 +1,6 @@
+export const Add = (a,b) => a + b;
+export const Multiply = (a,b) => a * b;
+export const Divide = (a,b) => a / b;
+export const Substract = (a,b) => a - b;
+export const ID = 23;
+

8f539ac8dde7a0c619cbd44699706e3bd69c7bff
Didier Ukanda <didier.ukanda@gmail.com>
2019-06-19 03:11:44 -0300

    Forlify: search  Object SetUp

--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/index.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/index.js
@@ -1,3 +1,6 @@
+import Search from './models/Search';
+
+//#region All 3 way to import modules
 // //! first way to import thing as simple files
 // import str from './models/Search'
 
@@ -8,20 +11,44 @@
 // import * as searchView  from './views/searchView'
 
 // console.log(`Using the Imported functions ${searchView.Add(searchView.ID, 2)}`)
+//#endregion
 
-import axios from 'axios'
-// todo  Forkify Start Up
-let apiKey, URL, query, proxy;
+//#region TESTING API
+// todo done for testing API
+// import axios from 'axios'
+// // todo  Forkify Start Up
+// let apiKey, URL, query, proxy;
 
 
 
-async function getResults(query) {
-    proxy = 'https://cors-anywhere.herokuapp.com/';
-    apiKey = 'da30482b2b44851038c25d64c6b944cc';
-    URL = `${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`;
-    const res = await axios(`${URL}`);
-    console.log(res);
+// async function getResults(query) {
+//     proxy = 'https://cors-anywhere.herokuapp.com/';
+//     apiKey = 'da30482b2b44851038c25d64c6b944cc';
+//     URL = `${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`;
+//     try {
+//         const res = await axios(`${URL}`);
+//         const recipes = res.data.recipes;
+//         console.log(res);
+//     } catch (error) {
+//         alert(error)
+//     }
     
-}
+// }
 
-getResults('pizza');
+// getResults('pizza');
+//#endregion
+
+
+/** Gobal State of App
+ * todo Search Object
+ * todo Current recipe object
+ * todo Shopping List object
+ * todo Liked recipes
+ */
+ 
+
+const search = new Search('pizza');
+
+search.getResults();
+
+
--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/models/Search.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/models/Search.js
@@ -1,1 +1,24 @@
-export default 'I am a exported String';
+import axios from 'axios';
+
+export default class Search {
+
+    constructor(query) {
+        this.query = query;        
+    }
+
+    async getResults() {
+        console.log('Starting');
+        
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const apiKey = 'da30482b2b44851038c25d64c6b944cc';
+        const URL = `${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${this.query}`;
+        try {
+            const res = await axios(`${URL}`);
+            this.result = res.data.recipes;
+            console.log(res);
+        } catch (error) {
+            alert(error)
+        }
+        
+    }
+}

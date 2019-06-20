c97306265a9e57c53facc79830f5e3fb35646957
Didier Ukanda <didier.ukanda@gmail.com>
2019-06-20 15:16:37 -0300

    NetNinja_JS

--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/index.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/index.js
@@ -1,42 +1,6 @@
 import Search from './models/Search';
-
-//#region All 3 way to import modules
-// //! first way to import thing as simple files
-// import str from './models/Search'
-
-// //! second way import thing as aliases
-// // import {Add as a , Multiply as m, Substract as s, Divide as d, ID} from './views/searchView'
-
-// //! Third way alias the Path
-// import * as searchView  from './views/searchView'
-
-// console.log(`Using the Imported functions ${searchView.Add(searchView.ID, 2)}`)
-//#endregion
-
-//#region TESTING API
-// todo done for testing API
-// import axios from 'axios'
-// // todo  Forkify Start Up
-// let apiKey, URL, query, proxy;
-
-
-
-// async function getResults(query) {
-//     proxy = 'https://cors-anywhere.herokuapp.com/';
-//     apiKey = 'da30482b2b44851038c25d64c6b944cc';
-//     URL = `${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`;
-//     try {
-//         const res = await axios(`${URL}`);
-//         const recipes = res.data.recipes;
-//         console.log(res);
-//     } catch (error) {
-//         alert(error)
-//     }
-    
-// }
-
-// getResults('pizza');
-//#endregion
+import * as SearchViews from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base'
 
 
 /** Gobal State of App
@@ -46,9 +10,42 @@ import Search from './models/Search';
  * todo Liked recipes
  */
  
+const state = {};
 
-const search = new Search('pizza');
+const controlSearch = async () => {
+    
+    // todo 1. Get query from the view
+    
+    const query = SearchViews.getInput();
+    console.log(query);
+    console.log(SearchViews.ID);
+    
+    if (query) {
+        // todo 2. New Search Object and add to the State
+        state.search = new Search(query);
+        
+        // todo 3. Prepare Ui for result
+        SearchViews.clearInput();
+        SearchViews.clearResult();
+        renderLoader(elements.searchRes)
+        
+        
+        // todo 4. Search for recipe
+        await state.search.getResults();
 
-search.getResults();
+        // todo 5. Render result on UI
+        //* console.log(state.search.result)
+        clearLoader();
+        SearchViews.renderResults(state.search.result);
+    }
+    
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+
 
 
--- Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/views/searchView.js
+++ Javascript/The_Complete_JS_Course_Jonas_Schetdman/9-forkify/src/js/views/searchView.js
@@ -1,6 +1,76 @@
-export const Add = (a,b) => a + b;
-export const Multiply = (a,b) => a * b;
-export const Divide = (a,b) => a / b;
-export const Substract = (a,b) => a - b;
+// export const Add = (a,b) => a + b;
+// export const Multiply = (a,b) => a * b;
+// export const Divide = (a,b) => a / b;
+// export const Substract = (a,b) => a - b;
+
+/**
+ * 1:
+f2f_url: "http://food2fork.com/view/46956"
+image_url: "http://static.food2fork.com/fruitpizza9a19.jpg"
+publisher: "The Pioneer Woman"
+publisher_url: "http://thepioneerwoman.com"
+recipe_id: "46956"
+social_rank: 100
+source_url: "http://thepioneerwoman.com/cooking/2012/01/fruit-pizza/"
+title: "Deep Dish Fruit Pizza"
+ */
+import { elements } from './base'
 export const ID = 23;
+
+
+export const getInput = () => elements.inputSearch.value;
+
+export const clearInput = () => elements.inputSearch.value = '';
+
+export const clearResult = () => elements.searchResultList.innerHTML = '';
+const limitRecipeTitle = (title, limit = 17) =>  {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if( acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0)
+        return `${newTitle.join(' ')} ...`;
+    } 
+    return title;
+}
+
+export const renderRecipe = recipe => {
+    const markup = `<li>
+                        <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+                            <figure class="results__fig">
+                                <img src="${recipe.image_url}" alt="Test">
+                            </figure>
+                            <div class="results__data">
+                                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                                <p class="results__author">${recipe.publisher}</p>
+                            </div>
+                        </a>
+                    </li>`;
+    elements.searchResultList.insertAdjacentHTML( 'beforeend', markup )
+}
+
+const createButton = (page, type) => {
+    ``
+}
+
+const renderButtons =  (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    if(page === 1 && pages > 1){
+        //Only button to go next page
+    } else if (page === pages) {
+        // only button to go previous page
+    }else {
+        // two buttons one to go next one to go previous page
+    }
+}
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = resPerPage * (page - 1);
+    const end = (resPerPage * page) - 1;
+    recipes.slice(start, end).forEach(renderRecipe);
+}
 

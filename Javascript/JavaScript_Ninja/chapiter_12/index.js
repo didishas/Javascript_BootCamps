ba00ca6812f3057e63230623632a5591a70f5137
didishas <didier.ukanda@gmail.com>
2019-06-22 12:05:39 -0300

    XMLHttprequest

---
+++ Javascript/JavaScript_Ninja/chapiter_12/index.js
@@ -0,0 +1,30 @@
+console.log('Starting')
+
+const request = new XMLHttpRequest();
+const url = 'https://jsonplaceholder.typicode.com/todos/';
+
+let container = document.querySelector('.container')
+
+
+
+request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState);
+
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request.responseText)
+
+        const data = request.responseText;
+        const info = JSON.parse(data);
+
+        container.textContent = '';
+        info.forEach((cur,index) => {
+            container.insertAdjacentHTML('afterbegin',`<h3>${cur.title}</h3>
+                        <p>completed: ${cur.completed}</p>`)
+        })
+    }
+})
+
+request.open('GET', url);
+request.send();
+
+

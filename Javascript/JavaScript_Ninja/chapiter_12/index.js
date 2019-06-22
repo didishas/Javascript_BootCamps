5577cd4de49f0981a9f1a00e804830bf5699b2ac
didishas <didier.ukanda@gmail.com>
2019-06-22 14:15:50 -0300

    async Method CallBack

--- Javascript/JavaScript_Ninja/chapiter_12/index.js
+++ Javascript/JavaScript_Ninja/chapiter_12/index.js
@@ -1,30 +1,31 @@
 console.log('Starting')
 
-const request = new XMLHttpRequest();
-const url = 'https://jsonplaceholder.typicode.com/todos/';
+const getTodos = () => {
+    const request = new XMLHttpRequest();
+    const url = 'https://jsonplaceholder.typicode.com/todos/';
 
-let container = document.querySelector('.container')
-
-
-
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-
-    if(request.readyState === 4 && request.status === 200){
-        console.log(request.responseText)
-
-        const data = request.responseText;
-        const info = JSON.parse(data);
-
-        container.textContent = '';
-        info.forEach((cur,index) => {
-            container.insertAdjacentHTML('afterbegin',`<h3>${cur.title}</h3>
-                        <p>completed: ${cur.completed}</p>`)
-        })
-    }
-})
-
-request.open('GET', url);
-request.send();
+    let container = document.querySelector('.container')
 
 
+
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
+
+        if(request.readyState === 4 && request.status === 200){
+            console.log(request.responseText)
+            const data = request.responseText;
+            const info = JSON.parse(data);
+
+            container.textContent = '';
+            info.forEach((cur,index) => {
+                container.insertAdjacentHTML('afterbegin',`<h3>${cur.title}</h3>
+                            <p>completed: ${cur.completed}</p>`)
+            })
+        }
+    })
+
+    request.open('GET', url);
+    request.send();
+} 
+
+getTodos()

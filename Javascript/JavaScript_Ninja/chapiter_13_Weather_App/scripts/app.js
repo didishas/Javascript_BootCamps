ba1fe7779deef50eb6ab827bccdb2660c52bbed9
didishas <didier.ukanda@gmail.com>
2019-06-23 14:13:02 -0300

    Ninja Weather App Final Version

--- Javascript/JavaScript_Ninja/chapiter_13_Weather_App/scripts/app.js
+++ Javascript/JavaScript_Ninja/chapiter_13_Weather_App/scripts/app.js
@@ -5,6 +5,9 @@ const card = document.querySelector('.card');
 const cityName = document.querySelector('h5.my-3');
 const temperature = document.getElementById('temp');
 const unit = document.getElementById('unit');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
 
 const getInputLocation = (e) => {
     const location = e.target.city.value;
@@ -28,6 +31,24 @@ const UpdatUI = function(details) {
     weatherCond.textContent = details.weather.WeatherText;
     unit.textContent += details.weather.Temperature.Metric.Unit;
 }
+
+const UpdateImg = function(details) {
+
+    // update the night/day & icon images
+    
+    const iconSrc = `img/icons/${details.weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc)
+    
+    let timeSrc = null;
+
+    if(details.weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc)
+}
+
 cityForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -37,8 +58,10 @@ cityForm.addEventListener('submit', (e) => {
     .then(details => {
         console.log(details);
         UpdatUI(details);
+        UpdateImg(details);
         if (card.classList.contains('d-none')) 
             card.classList.remove('d-none')
     })
 })
+
 

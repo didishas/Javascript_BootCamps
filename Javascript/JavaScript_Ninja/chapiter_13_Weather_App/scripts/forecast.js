7d4f691dfcec0cdd8519c6842bacde5686aee16a
didishas <didier.ukanda@gmail.com>
2019-06-23 08:44:47 -0300

    Ninja App v 1.0.0 : no modules all in code in forecast.js

--- Javascript/JavaScript_Ninja/chapiter_13_Weather_App/scripts/forecast.js
+++ Javascript/JavaScript_Ninja/chapiter_13_Weather_App/scripts/forecast.js
@@ -1,2 +1,34 @@
 const key = 'AiL4vZCiVqtPSBo6qTnvkaGqfdGzAhKv';
 
+const form = document.querySelector('.change-location');
+
+
+const getLocation = (e) => {
+    return e.target.city.value;
+}
+
+const getInfo = async (url) => {
+    const promiseInfo = await fetch(url);
+    const jsonInfos = promiseInfo.json();
+
+    return jsonInfos;
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const city = getLocation(e);
+
+    const cityBase =`http://dataservice.accuweather.com/locations/v1/cities/search`
+    const query = `?apikey=${key}&q=${city}&details=true`; 
+    
+    const urlLocation = `${cityBase}${query}`;
+
+    const weatherBase = `http://dataservice.accuweather.com/currentconditions/v1/`
+
+    const cityCode = getInfo(urlLocation)
+                    .then(data => data[0].Key)
+                    .then(cityKey => getInfo(`${weatherBase}${cityKey}?apikey=${key}&details=true`))
+                    .then(weatherInfo => console.log(weatherInfo[0]));
+})
+
+

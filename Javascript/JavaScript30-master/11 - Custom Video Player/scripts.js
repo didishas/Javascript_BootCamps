2edd37d7885248d926bd3c16050942790113455b
didishas <didier.ukanda@gmail.com>
2019-07-11 15:19:23 -0300

    JS 30 Custom Player my first try

--- Javascript/JavaScript30-master/11 - Custom Video Player/scripts.js
+++ Javascript/JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -1,21 +1,47 @@
 
-  const playBtn = document.querySelector('.player__button');
+  const togglePlayBtn = document.querySelector('.player__button.toggle');
   const video = document.querySelector('.player__video');
-
+  const playBtns = document.querySelectorAll('.player__button');
+  console.log(playBtns)
+  
   let isPlaying = false;
+  let totalTime;
   
   const playVid = function(){
-    // console.log('clicked')
-    isPlaying = !isPlaying;
-    video.play();
-} 
+      // console.log('clicked')
+      isPlaying = !isPlaying;
+      video.play();
+      if(!totalTime) totalTime = video.duration;
+    } 
+    
+    const pauseVid = () => {
+        isPlaying = !isPlaying;
+        video.pause();
+    }
+    
+    const SetBackward = (num) => {
+    video.currentTime += num;
+    console.log(num);
+    
+}
 
-const pauseVid = () => {
-    isPlaying = !isPlaying;
-      video.pause();
-  }
-
-  playBtn.addEventListener('click', (e) => {
-      if(!isPlaying) playVid();
+playBtns.forEach((playBtn, index) => {
+    if(index !== 0){
+        console.dir(playBtn.getAttribute('data-skip'))
+        playBtn.addEventListener('click', function (e) {
+            // console.log(parseInt(this.getAttribute('data-skip')));
+            SetBackward(parseInt(this.getAttribute('data-skip')));
+        })
+    }
+    
+})
+togglePlayBtn.addEventListener('click', (e) => {
+    if(!isPlaying) playVid();
       else pauseVid();
-  })
+    })
+    video.addEventListener('click', (e) => {
+        if(!isPlaying) playVid();
+        else pauseVid();
+    })
+    
+    

c23dfce511ef1b0a9a9033e5df01eb49933ab0bc
didishas <didier.ukanda@gmail.com>
2019-07-19 02:49:59 -0300

    Andrew Mead Notes-App Filter

--- Javascript/The Moder Js BootCamp by AndrewMead/notes-App/main.js
+++ Javascript/The Moder Js BootCamp by AndrewMead/notes-App/main.js
@@ -1,4 +1,7 @@
 const noteList = document.querySelector('.note-list');
+const createBtn = document.querySelector('#create-btn');
+const deleteBtn = document.querySelector('#delete-btn');
+const searchText = document.getElementById('search-text');
 
 const notes = [{
     title: 'My next trip',
@@ -14,9 +17,42 @@ const notes = [{
 const display = function(note) {
     const html = `
     <li>
-        <label for="titre">titre: ${note.title}</label>
+        <label for="titre">Titre: ${note.title}</label>
         <p>Body: ${note.body}</p>
     </li>`
     noteList.insertAdjacentHTML('beforeend', html)
 }
-notes.forEach(note => display(note));
+
+const removeNote = function(note) {
+    note.remove();
+}
+
+const renderNotes = function(notes) {
+    notes.forEach(note => display(note));
+}
+
+renderNotes(notes);
+
+createBtn.addEventListener('click', function(e){
+    display({
+        title: 'New Note',
+        body: 'New instructions'
+    })
+    console.log('A note is created.')
+})
+
+// deleteBtn.addEventListener('click', function(e){
+//     const docs = document.querySelectorAll('li')
+//     docs.forEach(doc => removeNote(doc))
+//     console.log('All notes are removed.')
+// })
+
+searchText.addEventListener('input', function(e){
+    console.log(e.target.value);
+    noteList.innerHTML = '';
+    const filteredNotes = notes.filter(note => {
+        return (note.body.toLowerCase().includes(e.target.value) || note.title.toLowerCase().includes(e.target.value))
+    });
+    
+    renderNotes(filteredNotes);
+})
--- Javascript/The Moder Js BootCamp by AndrewMead/todos-App/main.js
+++ Javascript/The Moder Js BootCamp by AndrewMead/todos-App/main.js
@@ -4,6 +4,12 @@ console.log('Starting ...');
 
 const paras = document.querySelectorAll('p');
 const body = document.querySelector('body');
+const sousTitre = document.querySelector('h3');
+const todoList = document.querySelector('section.todos');
+const newTodo = document.getElementById('new-todo-text');
+const button = document.querySelector('button');
+const searchTodo = document.querySelector('#search-todo-text')
+
 console.log(paras);
 
 paras.forEach(para => console.log(para.textContent));
@@ -15,14 +21,14 @@ const filteredTodos = function(paragraphs, word) {
         return para.textContent.toLowerCase().includes(word.toLowerCase());
     })
 }
-console.log(filteredTodos(paragraphs, 'the'));
+// console.log(filteredTodos(paragraphs, 'the'));
 
 const removeTodos = function(paragraphs, word){
     const tags = filteredTodos(paragraphs, word);
     tags.forEach(tag => tag.remove());
 }
 
-removeTodos(paragraphs, 'the')
+// removeTodos(paragraphs, 'the')
 
 let todos = [{
     text:'Walk in the park',
@@ -41,8 +47,42 @@ let todos = [{
         completed: false
     }];
 
+const renderTodos = function(todos) {
     todos.forEach(todo => {
-        const html = `<p>${todo.text}</p>`
-        body.insertAdjacentHTML('beforeend', html);
-        console.log(todo.text)
+        const html = `<p class="todo">${todo.text}</p>`;
+        todoList.insertAdjacentHTML('beforeend', html);
+        console.log(todo.text);
+    });
+}
+
+renderTodos(todos);
+
+const todosLeft = function() {
+    const numberOfUndos = todos.reduce(function(sumOfTodos, current){
+        if(!current.completed) sumOfTodos++;
+        return sumOfTodos;
+    }, 0)
+    
+    console.log(numberOfUndos);
+    
+    const p = document.createElement('p');
+    p.textContent = `You have ${numberOfUndos} todos left`;
+    sousTitre.insertAdjacentElement('afterend', p)
+}
+
+todosLeft();    
+
+button.addEventListener('click', function(e){
+    console.log(newTodo.value);
+    newTodo.value = '';
+})
+
+searchTodo.addEventListener('input', function(e){
+    console.log(e.target.value);
+    todoList.innerHTML = '';
+    
+    const searchedTodos = todos.filter(todo => {
+        return (todo.text.toLocaleLowerCase().includes(e.target.value.toLowerCase()));
+    })
+    renderTodos(searchedTodos)
 })
